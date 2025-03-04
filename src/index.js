917959f0a1b4397a4e61d2b24c33845dab13c657
dorothymuhonja <dorothymuhonja7@gmail.com>
2025-03-04 20:18:22 +0300

    modified html and css

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ function generatePoem(event) {
     let prompt = `User instructions: Generate a poem about ${instructionInput.value}`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="blink"> Generating a poem about ${instructionInput.value}</div>`;
+
     axios.get(apiUrl).then(displayPoem);
 
     
