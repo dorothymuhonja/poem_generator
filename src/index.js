793de2cc71cfe5a963df402e515c91b2f7540776
dorothymuhonja <dorothymuhonja7@gmail.com>
2025-03-04 19:40:33 +0300

    intergrate api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-function generatePoem(event) {
-    event.preventDefault();
-
+function displayPoem (response) {
     new Typewriter("#poem", {
-        strings: "Hereth goeth the poem",
+        strings: response.data.answer,
         autoStart: true,
         delay: 1,
         cursor: "",
     });
+}
+
+function generatePoem(event) {
+    event.preventDefault();
+
+    let instructionInput = document.querySelector("#user-instructions")
+
+    let apiKey = "e0ec6b874369tb90f96386aaf483o95a";
+    let context = "You are a haiku expert and enjoy writing poems. Your mission is to generate three line haikus in basic html and separate each line with a <br/>. Make sure to follow the user instructions. Sign the poem with  'SheCodes AI' inside a <strong> element. Do not include a title to the poem.";
+    let prompt = `User instructions: Generate a poem about ${instructionInput.value}`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(displayPoem);
+
+    
 
 }
 
